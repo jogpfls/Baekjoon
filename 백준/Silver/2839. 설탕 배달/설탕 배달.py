0eea6ab48e86f88e6f4606f336c29a7dcb6d59f5
조혜린 <164613169+jogpfls@users.noreply.github.com>
2025-02-01 15:34:29 +0900

    [Silver IV] Title: 설탕 배달, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.py
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -1,14 +1,14 @@
 N = int(input())
-count = 0
+cnt = 0
 
-while(True):
-    if N % 5 == 0:
-        count += N//5
-        print(count)
+while True:
+    if N % 5 != 0:
+        N -= 3 
+        cnt += 1
+    elif N % 5 == 0:
+        cnt += N // 5
+        print(cnt)
         break
-    N -= 3
-    count += 1
-
     if N < 0:
         print(-1)
         break
